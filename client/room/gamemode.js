fd637eb96b18962f113a89f793994983c3d35a0b
TɴT - GɪᴛHᴜʙ (PɪxᴇʟCᴏᴍʙᴀᴛs𝟸) <163316535+StroylsMaxPixelCombats@users.noreply.github.com>
2025-01-05 18:21:13 +0700

    Update gamemode.js

--- client/room/gamemode.js
+++ client/room/gamemode.js
@@ -32,6 +32,7 @@ BreackGraph.WeakBlocks = GameMode.Parameters.GetBool("LoosenBlocks");
 
 // Блок игрока, всегда - усилен:
 BreackGraph.PlayerBlockBoost = true;
+
 //Запрещаем, урон - гранатами:
 Damage.GetContext().GranadeTouchExplosion.Value = false;
 
@@ -96,7 +97,7 @@ Ui.GetContext().TeamProp2.Value = { Team: "Red", Prop: scoresProp };
 
 // Задаём, очки - в команде:
 for (arr = Teams.All; arr.MoveNext();) {
-	  arr.Current.Properties.Get(scoresProp).Value= 0;
+	  arr.Current.Properties.Get(scoresProp).Value = 0;
 }
 
 // Разрешаем игроку, зайти в - команду:
@@ -220,9 +221,7 @@ SetWaitingMode();
 function SetWaitingMode() {
 	stateProp.value = WaitingStateValue;
 	Ui.GetContext().Hint.Value = "Ожидание, игроков...";
-	 if (GameMode.Parameters.GetBool("En")) {
-	Ui.GetCotext().Hint.Value = "
-	Spawns.GetContext().enable = false;
+	Spawns.GetContext().Enable = false;
 	TeamsBalancer.IsAutoBalance = true;
 	mainTimer.Restart(WaitingModeSeconts);
 }
@@ -230,7 +229,7 @@ function SetWaitingMode() {
 function SetBuildMode() 
 {
 	stateProp.value = BuildModeStateValue;
-	Ui.GetContext().Hint.Value = "Hint/BuildBase";
+	Ui.GetContext().Hint.Value = "!Застраивайте, базу и - атакуйте, врагов!";
 
 	var inventory = Inventory.GetContext();
 	inventory.Main.Value = false;
@@ -240,15 +239,15 @@ function SetBuildMode()
 	inventory.Build.Value = true;
 
 	mainTimer.Restart(BuildModeSeconds);
-	Spawns.GetContext().enable = true;
-	TeamsBalancer.IsAutoBalance = true; // âêë àâòîáàëàíñ äî íà÷àëà êàòêè
+	Spawns.GetContext().Enable = true;
+	TeamsBalancer.IsAutoBalance = true; 
 	SpawnTeams();
 }
 function SetGameMode() 
 {
 	stateProp.value = GameStateValue;
-	Ui.GetContext().Hint.Value = "Hint/AttackEnemies";
-	winTeamIdProp.Value = null; // íèêòî íå âûèãðàë
+	Ui.GetContext().Hint.Value = "!Атакуйте, врагов!";
+	winTeamIdProp.Value = null;
 
 	var inventory = Inventory.GetContext();
 	if (GameMode.Parameters.GetBool("OnlyKnives")) {
@@ -273,21 +272,20 @@ function SetGameMode()
 	SpawnTeams();
 }
 
-function StartEndOfGame(team) { // team=null òî íè÷üÿ
+function StartEndOfGame(team) { 
 	log.debug("win team="+team);
 	stateProp.value = EndOfGameStateValue;
 	if (team !== null) {
 		log.debug(1);
-		Ui.GetContext().Hint.Value = team + " wins!";
+		Ui.GetContext().Hint.Value = `team + wins!`;
 		 var prop = team.Properties.Get(scoresProp);
 		 if (prop.Value == null) prop.Value = 1;
 		 else prop.Value = prop.Value + 1;
 	}
 	else Ui.GetContext().Hint.Value = "Hint/Draw";
-
 	mainTimer.Restart(EndGameSeconds);
 }
-function EndEndOfGame(){// êîíåö êîíöà ìàò÷à
+function EndEndOfGame() {
 	if (winTeamIdProp.Value !== null) {
 		var team = Teams.Get(winTeamIdProp.Value);
 		var prop = team.Properties.Get(scoresProp);
@@ -299,11 +297,11 @@ function EndEndOfGame(){// êîíåö êîíöà ìàò÷à
 
 function SetEndOfMatchMode() {
 	stateProp.value = EndOfMatchStateValue;
-	Ui.GetContext().Hint.Value = "Hint/EndOfMatch";
+	 Ui.GetContext().Hint.Value = "!Конец, матча!";
 
-	var context = Spawns.GetContext();
-	context.enable = false;
-	context.Despawn();
+	Spawns.GetContext().Enable = false;
+	Spawns.GetContext().Despawn();
+	
 	Game.GameOver(LeaderBoard.GetTeams());
 	mainTimer.Restart(EndOfMatchTime);
 }
@@ -312,9 +310,9 @@ function RestartGame() {
 }
 
 function SpawnTeams() {
-	var e = Teams.GetEnumerator();
-	while (e.moveNext()) {
-		Spawns.GetContext(e.Current).Spawn();
+	var Spawns = Spawns.GetContext();
+         Spawns.Spawn();
+	  SpawnTeams();
 	}
 }
 
